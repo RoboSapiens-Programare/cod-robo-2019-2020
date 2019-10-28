11d0334f7016fc0fff3ce374d7303fbf6302eecc
PC Robotica <------------->
2019-10-28 12:43:34 +0200

    Added TelemetryOut and stopped tracking build.gradle and gradle-wrapper.properties

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryOut.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorMRRangeSensor;
+
+@Autonomous(name = "Telemetry")
+public final class TelemetryOut extends LinearOpMode {
+    ModernRoboticsI2cRangeSensor range1 = null;
+    ModernRoboticsI2cRangeSensor range2 = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        range1= hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range1");
+        range2 = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range2");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(range1 != null && range2 != null) {
+                telemetry.addData("Range1: ", range1.rawUltrasonic());
+                telemetry.addData("Range2: ", range2.rawUltrasonic());
+                telemetry.update();
+            }
+        }
+    }
+}
