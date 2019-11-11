b2ea9229274701a50db3bc3c7e7fed8d515566c3
PC Robotica <------------->
2019-11-11 16:03:26 +0200

    test culoare done :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCuloare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCuloare.java
@@ -1,21 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.adafruit.AdafruitI2cColorSensor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+ @Autonomous (name = "TestCuloare")
 
-public class TestCuloare extends LinearOpMode {
+public class  TestCuloare extends LinearOpMode {
 
-    ColorSensor Color = null;
+     ModernRoboticsI2cColorSensor Color;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Color = hardwareMap.colorSensor.get("color");
-        Color.enableLed(true);
+        Color = hardwareMap.get(ModernRoboticsI2cColorSensor.class, "color");
+        Color.enableLed(false);
 
         waitForStart();
         while (opModeIsActive()) {
-            telemetry.addData("Nr culoare: ", Color.argb());
+            telemetry.addData("argb: ", Color.argb());
+            telemetry.addData("alpha: ", Color.alpha());
+            telemetry.addData("red: ", Color.red());
+            telemetry.addData("green: ", Color.green());
+            telemetry.addData("blue: ", Color.blue());
+            telemetry.addData("numar: ", Color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER));
             telemetry.update();
         }
 
