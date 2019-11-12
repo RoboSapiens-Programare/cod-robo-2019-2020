4cb76d41e356d190ba04636ec93dfdba5ed66be2
PC Robotica <------------->
2019-11-12 16:53:08 +0200

    test motor :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Proto_Brat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Proto_Brat.java
@@ -3,25 +3,30 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "Prototip Servo Brat")
 public final class Proto_Brat extends LinearOpMode {
-    Servo ServoBrat = null;
+    //Servo ServoBrat = null;
+    DcMotor motor = null;
+    double DEADZONE = 0.1;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ServoBrat = hardwareMap.servo.get("ServoL");
-
+        //ServoBrat = hardwareMap.servo.get("ServoL");
+        motor = hardwareMap.dcMotor.get("motor");
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
 
         while(opModeIsActive()){
-            if(gamepad1.a){
-                ServoBrat.setPosition(0);
-            }
-            else if(gamepad1.b){
-                ServoBrat.setPosition(1);
-            }
+            if(Math.abs(gamepad1.left_stick_y) > DEADZONE){
+                //ServoBrat.setPosition(0);
+                motor.setPower(Range.clip(gamepad1.left_stick_y, -0.9, 0.9));
+            } else motor.setPower(0);
         }
     }
 }
