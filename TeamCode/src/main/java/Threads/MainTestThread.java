d7c3d4a2acba2c69b52d7796388e48be52a6a884
Robo-Sapiens Laptop Proiectare <robosapienscnshb@gmail.com>
2019-12-21 16:30:04 +0200

    start multithreading

---
+++ TeamCode/src/main/java/Threads/MainTestThread.java
@@ -0,0 +1,10 @@
+package Threads;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MainTestThread extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
